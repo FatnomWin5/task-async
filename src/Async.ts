062439e05e80614c2cf472451eab8e64f6ffc027
FantomWin5 <danya.paklin18@gmail.com>
2022-06-07 22:05:14 +0300

    задание по task-async

--- src/Async.ts
+++ src/Async.ts
@@ -3,6 +3,9 @@
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
 export function mock(ms: number): Promise<number> {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(ms), ms);
+    });
 }
 
 /*
@@ -11,7 +14,8 @@ export function mock(ms: number): Promise<number> {
 export function getData(): Promise<number[]> {
     const result: number[] = [];
 
-    return mock(100)
+    return Promise.all([mock(100), mock(200), mock(300)]);
+    /*return mock(100)
         .then((data1) => {
             result.push(data1);
             return mock(200);
@@ -23,7 +27,7 @@ export function getData(): Promise<number[]> {
         .then((data3) => {
             result.push(data3);
             return result;
-        });
+        });*/
 }
 
 /*
@@ -31,9 +35,12 @@ export function getData(): Promise<number[]> {
 завершенный с ошибкой Promise и возвращал текст ошибки.
  */
 export async function catchException(): Promise<string | undefined> {
-    try {
+    /*try {
         Promise.reject(new Error('my error'));
     } catch (err) {
         return err.message;
-    }
+    }*/
+    return Promise.reject().catch(() => {
+        return 'my error';
+    });
 }
